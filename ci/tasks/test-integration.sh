d310a571e5c615bec10347424e504efc08acb489
Justin Carter <justin@misthos.io>
2020-09-16 19:57:21 +0000

    Add cepler to path in integration test

--- ci/tasks/test-integration.sh
+++ ci/tasks/test-integration.sh
@@ -5,6 +5,8 @@ set -eux
 export CARGO_HOME="$(pwd)/cargo-home"
 export CARGO_TARGET_DIR="$(pwd)/cargo-target-dir"
 
+export PATH="${PATH}:${CARGO_TARGET_DIR}/debug/"
+
 pushd repo
 
 make integration
